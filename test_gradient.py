a9925b28d6bd9fe6907b37d193a0a1ec42be549d
Caleb Sanders <cpsanders@mymail.mines.edu>
2021-04-15 15:48:44 -0600

    test gradient file running

--- test_gradient.py
+++ test_gradient.py
@@ -5,24 +5,48 @@ Created April 13 2021
 
 This file compares the gradient computed in QNADE with the manually calculated theoretical gradient.
 """
+import torch
+import math 
+import torch.nn as nn
+import numpy as np 
+import matplotlib.pyplot as plt 
+from utils import generate_possible_states
+from QNADE import QNADE
 
+# input layer size
+L_in=2
+
+# hidden layer size
+H = 2*L_in
+
+# number of samples 
+batch_size = 2
+
+# Initialize network and model 
+FFNN = nn.Sequential(nn.Linear(L_in,H), nn.Tanh(), nn.Linear(H,2), nn.Tanh())
+model = QNADE(FFNN)
+
+# generate samples, psis, and grads 
+WAVS, samples, grads_per_param = model(N_samples=batch_size)
+
+# select a specific psi/grad to look at 
 returned_grad = grads_per_param[0][0][0][0]
-print(returned_grad)
 psi_sampled = WAVS[0]
 state = samples[0]
-epsilon = 0.001
+delta = 0.001 #amount by which to adjust a certain parameter 
 
 print("\nReturned Gradient Element: {}".format(returned_grad))
 
-# adjust network parameters slightly 
+# adjust network parameter slightly 
 params = list(FFNN.parameters())
 with torch.no_grad():
-  params[0][0][0].copy_(params[0][0][0] + epsilon)
+  params[0][0][0].copy_(params[0][0][0] + delta)
 
-# select the state you sampled and generate it's wavefunction coeff
-psi_adj_list, samples_adj, grads_adj, param_grads_adj = model(N_samples=None, x=samples)
+# generate new wavefunction coeff for a specified state based on adjusted param 
+psi_adj_list, samples_adj, param_grads_adj = model(N_samples=None, x=samples)
 psi_adj = psi_adj_list[0]
 
-manual_grad = (math.log(psi_adj) - math.log(psi_sampled))/epsilon
+# calculate gradient based on definition 
+manual_grad = (math.log(psi_adj) - math.log(psi_sampled))/delta
 
 print("Manually Computed Gradient Element: {}\n".format(manual_grad))
