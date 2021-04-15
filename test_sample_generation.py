6112177dd0697bd68f5ec829ee99021ba1192e88
Caleb Sanders <cpsanders@mymail.mines.edu>
2021-04-15 16:05:16 -0600

    test sample generation runs

--- test_sample_generation.py
+++ test_sample_generation.py
@@ -7,55 +7,44 @@ This file tests the generation of samples according to the NADE algorithm.
 Samples are generated and plotted alongside their theoretical distribution. 
 """
 
-def generate_possible_states(n):
-    """
-    Generate a numpy array of possible states for a system of n qubits 
-    """
+import torch 
+import torch.nn as nn 
+import numpy as np 
+import matplotlib.pyplot as plt
+from QNADE import QNADE
+from utils import generate_possible_states
 
-    # 2^(n-1)  2^n - 1 inclusive
-    bin_arr = range(0, int(math.pow(2,n)))
-    bin_arr = [bin(i)[2:] for i in bin_arr]
+L_in=5
+H = 2*L_in
+batch_size = 1000
 
-    # Prepending 0's to binary strings
-    max_len = len(max(bin_arr, key=len))
-    bin_arr = [i.zfill(max_len) for i in bin_arr]
+# Initialize network and model 
+FFNN = nn.Sequential(nn.Linear(L_in,H), nn.Tanh(), nn.Linear(H,2), nn.Tanh())
+model = QNADE(FFNN)
 
-    possible_states = []
-    for bit_string in bin_arr:
-        state = []
-        for char in bit_string:
-            bit = float(char)
-            if bit == 0.0:
-                state.append(-1.0)
-            else:
-                state.append(bit)
-        possible_states.append(state)
-
-    return np.array(possible_states)
-
-
+# generate samples, psis, grads, and probabilities  
+WAVS, samples, grads_per_param = model(N_samples=batch_size)
 probs = WAVS**2
 
+# generate all theoretical possible states 
 possible_states = generate_possible_states(L_in)
 bins = np.arange(start=1, stop=(2**L_in)+1, step=1)
 
 sampled_hist_data = [] #sampled state counts for each possible states 
 sample_prob_indices = [0]*len(possible_states)
 
-# re-bin and organize the sampled data 
+# re-bin and organize the sampled data for plotting
 for pos_state in range(len(possible_states)):
-
     count = 0
     for state in range(len(samples)):
         if np.array_equal(samples[state], possible_states[pos_state]):
             count += 1
-            sample_prob_indices[pos_state] = probs[state]
-
+            sample_prob_indices[pos_state] = probs[state].numpy()
+            
     sampled_hist_data.append(float(count))
 
-sampled_hist_data = np.asanyarray(sampled_hist_data)
-sampled_hist_data /= sum(sampled_hist_data)
-
+sampled_hist_data = np.asanyarray((sampled_hist_data))
+sampled_hist_data /= sum(sampled_hist_data) 
 
 plt.bar(bins, sampled_hist_data, alpha=0.3, label='samples')
 plt.scatter(bins, sample_prob_indices, label='|Psi|^2')
@@ -63,5 +52,4 @@ plt.title("System Size: {}".format(L_in))
 plt.xlabel("Possible States")
 plt.ylabel("Normalized Sample Count")
 plt.legend()
-plt.savefig("sample_gen_ex.png")
 plt.show()
